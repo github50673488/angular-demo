a9a2081b88ccf0b113577a75c1d1e76bf3446bfa
liuguojun <gj.liu@sj-sol.com>
2021-06-21 03:50:06 +0900

    0621

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular-liu';
 
-
   identity<T>(arg: T): T {
     return arg;
   }
 
   onClick1(): void {
     const a = this.identity('hello world');
-    console.log('a -->', a);
+    console.log('a -->F:\\AngularProject\\angular-demo', a);
 
     const b = this.identity(9527);
     console.log('b -->', b);
